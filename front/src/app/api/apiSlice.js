94f9ec8b161a6fb6c4d1d171f4b7afdeb0faea25
Vitor Müller <85710199+vitox013@users.noreply.github.com>
2023-03-10 13:41:15 -0300

    updated hostRestAPI

--- front/src/app/api/apiSlice.js
+++ front/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "https://sellanne-api.onrender.com",
+    baseUrl: "https://sellanne-production.up.railway.app/",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token;
