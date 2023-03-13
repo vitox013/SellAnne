1bfb635a462b16658f3b9fa49237ca9ca94eeee7
Vitor Müller <85710199+vitox013@users.noreply.github.com>
2023-03-13 04:31:31 -0300

    updated urlapi

--- front/src/app/api/apiSlice.js
+++ front/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "https://sell-anne.vercel.app/",
+    baseUrl: process.env.API_URL,
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token;
